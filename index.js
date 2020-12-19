8e7b63c1bb701dc3f7d9e66d942b47d94070d3c3
Lena Hou <lenahou2013@gmail.com>
2020-12-19 19:57:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,15 @@ function keepGoing(array, changeValue, skipValue){
   return array;
 }
 
+
+fucntion findFn() {
+const c = Math.floor(Math.random() * 100);
+for (let i = 0; i < array.length; i++){
+  if(array[i] === c);
+  return array[i];
+}
+}
+
 function findBy(array, findFn) {
   for(let i = 0; i < array.length; i++) {
     if(findFn(array[i])) {
@@ -27,7 +36,3 @@ function findBy(array, findFn) {
     }
   }
 }
-
-const c = Math.floor(Math.random() * 100);
-
-findBy([1, 1, 1, 1, c, 1, 2], (n) => n === c);
