65a41ff62f72f96200e949b6ab875d2adac6509b
Lena Hou <lenahou2013@gmail.com>
2020-12-19 19:33:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 function breakOut(array, changeValue, stopValue){
   for(let i = 0; i < array.length; i++) {
-    if(array[i] !== stopValue) {
       array[i] = changeValue;
-    } else if (array[i] === stopValue){
+    if (array[i] === stopValue){
       break;
     }
   }
