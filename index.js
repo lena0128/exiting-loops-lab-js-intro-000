ecf4fda556d3351f4d5b952739d92295ae058090
Lena Hou <lenahou2013@gmail.com>
2020-12-19 19:32:50 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function breakOut(array, changeValue, stopValue){
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] !== stopValue) {
+      array[i] = changeValue;
+    } else if (array[i] === stopValue){
+      break;
+    }
+  }
+}
